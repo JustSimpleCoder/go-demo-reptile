00c2c23c08d060b520104860e057458be3d4bd09
yaoooobin <yaobin@xdf.cn>
2020-05-13 18:45:07 +0800

    __ go

--- bussiness/persist/itemsaver.go
+++ bussiness/persist/itemsaver.go
@@ -1,22 +1,40 @@
 package persist
 
-import "log"
+import (
+	"context"
+	"github.com/olivere/elastic/v7"
+	"log"
+	"strings"
+)
 
 func ItemSaver() chan interface{} {
-
 	out := make(chan interface{})
-
 	go func() {
 
 		itemCount := 0
 		for {
 			item := <-out
-
 			itemCount++
 			log.Printf("Item saver %d : %v", itemCount, item)
+			save(item)
 		}
 
 	}()
-
 	return out
+}
+
+func save(item interface{}) {
+
+	client, err := elastic.Client{elastic.SetSniff(false)}
+	if err != nil {
+		panic(err)
+	}
+	response, err := client.Index().Index("reptile").Type("paper").BodyJson(item).Do(context.Background())
+
+	if err != nil {
+
+		panic(err)
+	}
+
+	log.Printf("%v", response)
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,8 @@ module go-demo-reptile
 go 1.12
 
 require (
-	github.com/gpmgo/gopm v0.6.5 // indirect
-	github.com/yuin/goldmark v1.1.30 // indirect
-	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
+	github.com/elastic/go-elasticsearch/v7 v7.6.0 // indirect
+	github.com/olivere/elastic/v7 v7.0.15
 	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f
-	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
-	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
 	golang.org/x/text v0.3.2
-	golang.org/x/tools v0.0.0-20200507205054-480da3ebd79c // indirect
 )
